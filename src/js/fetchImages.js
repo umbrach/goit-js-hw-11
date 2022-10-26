0761957beed4538b8474a9be1f0f4290f0603e2a
Denys Barabanov <umbrach415@gmail.com>
2022-10-26 16:16:53 +0300

    Update fetchImages.js

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
+
+
 export default async function fetchImages(value, page) {
   const url = 'https://pixabay.com/api/';
-  const key = '30848188-838038a8b40bb43baa3f98f48';
+  const key = '30677985-a8441cd36152dd6647e9ae9a3';
   const filter = `key=${key}&q=${value}&image_type=photo&min_width=800&orientation=horizontal&safesearch=true&per_page=40&page=${page}`;
-
   return await axios.get(`${url}?${filter}`).then(response => response.data);
 }
